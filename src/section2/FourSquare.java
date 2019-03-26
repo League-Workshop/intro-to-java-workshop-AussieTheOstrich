bb51fba753d4e2068a98175e508dac1c72d38aeb
alvincvms <>
2019-03-26 14:24:32 -0700

    Day 2

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,20 +6,26 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
+	Robot humpbackwhale = new Robot();
 
 	void go() {
 		// 4. Make the robot move as fast as possible
+		humpbackwhale.setSpeed(100);
 
 		// 5. Set the pen width to 5
+		humpbackwhale.setPenWidth(5);
 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+         for(int i=0;i<4;i++) {
 			// 7. Set the pen color to random
+         humpbackwhale.setRandomPenColor();
 	
 			// 1. Call the drawSquare() method
+		 drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
-
+            humpbackwhale.turn(90);
+	}
 	}
 
 	
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,6 +8,16 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+		Robot humpbackwhale = new Robot();
+	
+		humpbackwhale.penDown();
+		humpbackwhale.setSpeed(200);
+		humpbackwhale.move(200);
+		humpbackwhale.turn(120);
+		humpbackwhale.move(350);
+		humpbackwhale.turn(150);
+		humpbackwhale.move(400);
+		
 	
 		
 		
