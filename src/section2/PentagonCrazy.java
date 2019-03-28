f7e085bba95f2db84e07344f8df5ff9aa4dfa7f6
varnit05 <League@AdministorsiMac.attlocal.net>
2019-03-28 14:28:39 -0700

    Day 4

--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -38,8 +38,7 @@ humpbackwhale.turn(1);
 	// *13. randomize the color of the pattern 
 	// *14. experiment with different shapes
 
-	public static void main(String[] args); {
+
 		new PentagonCrazy().makePrettyThings();
 	}
-}
 }
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -8,18 +8,25 @@ public class QuizGame {
 		
 		// 1.  Create a variable to hold the user's score 
 		int score = 0;
-		System.out.println();
+
 		// 2.  Ask the user a question 
-		String name = JOptionPane.showInputDialog("Why did the Chicken cross the road?");
+		String name = JOptionPane.showInputDialog("whats heavy forwards but backwards its not?");
 		// 3.  Use an if statement to check if their answer is correct
-		if(name.equals("to die")) {score ++;} 
+		if(name.equals("ton"))  {
 		// 4.  if the user's answer is correct
 		
-		// -- add one to their score 
-		
+		// -- add one to their score           
+		score ++;}
+		System.out.println(score);
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		String yeet = JOptionPane.showInputDialog("knock knock?");
+		if(yeet.equals("whos there"))  {
+			score ++;}
+		System.out.println(score);
+		System.out.println("WOW YOU HAVE FINISHED!");
+		JOptionPane.showMessageDialog(null, "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo " );
+		}
 		// 6.  After all the questions have been asked, print the user's score 
 		
 	}
-}  
+
